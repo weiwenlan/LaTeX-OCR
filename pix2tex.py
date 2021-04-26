a3f9a943195d3b1a6c669adb13bd623a22a0f445
WEI <weiwenlan1999@gmail.com>
2021-04-26 13:49:12 +0800

    4.26 First commit

--- pix2tex.py
+++ pix2tex.py
@@ -14,20 +14,11 @@ import torch
 from torchvision import transforms
 from munch import Munch
 from transformers import PreTrainedTokenizerFast
-from timm.models.resnetv2 import ResNetV2
-from timm.models.layers import StdConv2dSame
+
 
 from dataset.latex2png import tex2pil
 from models import get_model
 from utils import *
-
-
-def minmax_size(img, max_dimensions):
-    ratios = [a/b for a, b in zip(img.size, max_dimensions)]
-    if any([r > 1 for r in ratios]):
-        size = np.array(img.size)//max(ratios)
-        img = img.resize(size.astype(int), Image.BILINEAR)
-    return img
 
 
 def initialize(arguments):
@@ -40,40 +31,60 @@ def initialize(arguments):
 
     model = get_model(args)
     model.load_state_dict(torch.load(args.checkpoint, map_location=args.device))
-
-    if 'image_resizer.pth' in os.listdir(os.path.dirname(args.checkpoint)) and not arguments.no_resize:
-        image_resizer = ResNetV2(layers=[2, 3, 3], num_classes=22, global_pool='avg', in_chans=1, drop_rate=.05,
-                                 preact=True, stem_type='same', conv_layer=StdConv2dSame).to(args.device)
-        image_resizer.load_state_dict(torch.load(os.path.join(os.path.dirname(args.checkpoint), 'image_resizer.pth'), map_location=args.device))
-        image_resizer.eval()
-    else:
-        image_resizer = None
     tokenizer = PreTrainedTokenizerFast(tokenizer_file=args.tokenizer)
-    return args, model, image_resizer, tokenizer
+    return args, model, tokenizer
 
-
-def call_model(args, model, image_resizer, tokenizer):
+def call_model_local(args, model, tokenizer):
     encoder, decoder = model.encoder, model.decoder
-    img = pad(ImageGrab.grabclipboard())
+    #img = ImageGrab.grabclipboard()
+    #img = Image.open('latex1.png')
+    img = Image.open(args.name)
     if img is None:
         print('Copy an image into the clipboard.')
         return
-    img = minmax_size(img, args.max_dimensions)
-    if image_resizer is not None:
-        with torch.no_grad():
-            input_image = pad(img, args.patch_size).convert('RGB').copy()
-            r, w = 1, img.size[0]
-            for i in range(10):
-                img = minmax_size(input_image.resize((w, int(input_image.size[1]*r)), Image.BILINEAR if r > 1 else Image.LANCZOS), args.max_dimensions)
-                t = test_transform(image=np.array(pad(img).convert('RGB')))['image'][:1].unsqueeze(0)
-                w = image_resizer(t.to(args.device)).argmax(-1).item()*32
-                if (w/img.size[0] == 1):
-                    break
-                r *= w/img.size[0]
-    else:
-        img = np.array(pad(img, args.patch_size).convert('RGB'))
-        t = test_transform(image=img)['image'][:1].unsqueeze(0)
+    
+    print(img.size)
+    if img.size[1]>=100:
+        zoom = 50/img.size[1]
+        img = img.resize((int(img.size[0]*zoom), int(img.size[1]*zoom)), Image.ANTIALIAS)
+    print(img.size)
+    ratios = [a/b for a, b in zip(img.size, args.max_dimensions)]
+    if any([r > 1 for r in ratios]):
+        size = np.array(img.size)//max(ratios)
+        img = img.resize(size.astype(int), Image.BILINEAR)
+    img = np.array(pad(img, args.patch_size).convert('RGB'))
+    t = test_transform(image=img)['image'][:1].unsqueeze(0)
+    im = t.to(args.device)
 
+    with torch.no_grad():
+        model.eval()
+        device = args.device
+        encoded = encoder(im.to(device))
+        dec = decoder.generate(torch.LongTensor([args.bos_token])[:, None].to(device), args.max_seq_len,
+                               eos_token=args.eos_token, context=encoded.detach(), temperature=args.temperature)
+        pred = post_process(token2str(dec, tokenizer)[0])
+    print(pred, '\n')
+    # df = pd.DataFrame([pred])
+    
+    # try:
+    #     tex2pil([f'$${pred}$$'])[0].show()
+    # except Exception as e:
+    #     print(e)
+
+
+def call_model(args, model, tokenizer):
+    encoder, decoder = model.encoder, model.decoder
+    #img = ImageGrab.grabclipboard()
+    img = Image.open('latex1.png')
+    if img is None:
+        print('Copy an image into the clipboard.')
+        return
+    ratios = [a/b for a, b in zip(img.size, args.max_dimensions)]
+    if any([r > 1 for r in ratios]):
+        size = np.array(img.size)//max(ratios)
+        img = img.resize(size.astype(int), Image.BILINEAR)
+    img = np.array(pad(img, args.patch_size).convert('RGB'))
+    t = test_transform(image=img)['image'][:1].unsqueeze(0)
     im = t.to(args.device)
 
     with torch.no_grad():
@@ -100,7 +111,7 @@ if __name__ == "__main__":
     parser.add_argument('-m', '--checkpoint', type=str, default='checkpoints/weights.pth')
     parser.add_argument('-s', '--show', action='store_true', help='Show the rendered predicted latex code')
     parser.add_argument('--no-cuda', action='store_true', help='Compute on CPU')
-    parser.add_argument('--no-resize', action='store_true', help='Resize the image beforehand')
+    parser.add_argument('-n','--name', type=str, help='file_name')
     args = parser.parse_args()
     logging.getLogger().setLevel(logging.FATAL)
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -109,12 +120,11 @@ if __name__ == "__main__":
         sys.path.insert(0, latexocr_path)
         os.chdir(latexocr_path)
 
-    args, *objs = initialize(args)
-    while True:
-        instructions = input('Press ENTER to predict the LaTeX code for the image in the memory. Type "x" to stop the program. ')
-        if instructions.strip().lower() == 'x':
-            break
-        try:
-            call_model(args, *objs)
-        except KeyboardInterrupt:
-            pass
+    args, model, tokenizer = initialize(args)
+    call_model_local(args, model, tokenizer)
+    # while True:
+    #     instructions = input('Press ENTER to predict the LaTeX code for the image in the memory. Type "x" to stop the program. ')
+    #     if instructions.strip().lower() == 'x':
+    #         break
+    #     #call_model(args, model, tokenizer)
+    #     call_model_local(args, model, tokenizer)
